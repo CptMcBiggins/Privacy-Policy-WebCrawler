af2dc2700b323be120ff56ca7e9ca9006d23d5c2
CptMcBiggins <72402633+CptMcBiggins@users.noreply.github.com>
2021-02-26 20:05:48 -0500

    Update crawl.py

--- crawl.py
+++ crawl.py
@@ -12,7 +12,7 @@ from oauth2client.service_account import ServiceAccountCredentials
 #args = parser.parse_args()
 
 #URL = args.u
-URL = 'https://anchor.fm/privacy'
+URL = 'http://talkandcomment.com/legal/privacy'
 page = requests.get(URL)
 soup = BeautifulSoup(page.content, 'html.parser')
 scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
@@ -23,23 +23,30 @@ sheet = client.open("Test Sheet").sheet1
 def main():
 	def coppa():
 		results_list = []
-		childp = soup.find_all('p', string = re.compile("child"))
-		for i in childp:
-			if i is not None:
-				results = (i.text.strip())
-				results_list.append(results)
+		
+		coppap = soup.find_all('p')
+		for i in coppap:
+			results = str(i.text.strip())
+			if 'child' in results:
+				clean = str(results.replace("\n",","))
+				super_clean = re.sub("[  ] ", "", clean)
+				results_list.append(super_clean)
 
 		coppali = soup.find_all('li')
 		for i in coppali:
 			results = str(i.text.strip())
 			if 'child' in results:
-				results_list.append(results)	
+				clean = str(results.replace("\n",","))
+				super_clean = re.sub("[  ] ", "", clean)
+				results_list.append(super_clean)
 
 		coppatd = soup.find_all('td')
 		for i in coppatd:
 			results = str(i.text.strip())
 			if 'child' in results:
-				results_list.append(results)	
+				clean = str(results.replace("\n",","))
+				super_clean = re.sub("[  ] ", "", clean)
+				results_list.append(super_clean)	
 
 		listToStr = ' '.join([str(i) for i in results_list])
 		active_cell = sheet.findall(URL)
@@ -53,53 +60,67 @@ def main():
 
 	def p1():
 		results_list = []
-		collectp = soup.find_all('p', string = re.compile("collect"))
-		for i in collectp:
-			if i is not None:
-				results = (i.text.strip())
-				results_list.append(results)	
+
+		p1p = soup.find_all('p')
+		for i in p1p:
+			results = str(i.text.strip())
+			if 'collect' in results:
+				clean = str(results.replace("\n",","))
+				super_clean = re.sub("[  ] ", "", clean)
+				results_list.append(super_clean)	
 
 		p1li = soup.find_all('li')
 		for i in p1li:
 			results = str(i.text.strip())
 			if 'collect' in results:
-				results_list.append(results)	
+				clean = str(results.replace("\n",","))
+				super_clean = re.sub("[  ] ", "", clean)
+				results_list.append(super_clean)	
 
 		p1td = soup.find_all('td')
 		for i in p1td:
 			results = str(i.text.strip())
 			if 'collect' in results:
-				results_list.append(results)	
+				clean = str(results.replace("\n",","))
+				super_clean = re.sub("[  ] ", "", clean)
+				results_list.append(super_clean)	
 
 		listToStr = ' '.join([str(i) for i in results_list])
 		active_cell = sheet.findall(URL)
 		for i in active_cell:
 			url_row = i.row
 			row_str = str(url_row)	
-
+		
 		active_row = 'H' + row_str
 		sheet.update(active_row, listToStr)
 	p1()
 	
 	def p2():
 		results_list = []
-		usep = soup.find_all('p', string = re.compile("use "))
-		for i in usep:
-			if i is not None:
-				results = (i.text.strip())
-				results_list.append(results)	
+		
+		p2p = soup.find_all('p')
+		for i in p2p:
+			results = str(i.text.strip())
+			if 'use ' in results:
+				clean = str(results.replace("\n",","))
+				super_clean = re.sub("[  ] ", "", clean)
+				results_list.append(super_clean)
 
 		p2li = soup.find_all('li')
 		for i in p2li:
 			results = str(i.text.strip())
 			if 'use ' in results:
-				results_list.append(results)	
+				clean = str(results.replace("\n",","))
+				super_clean = re.sub("[  ] ", "", clean)
+				results_list.append(super_clean)	
 
 		p2td = soup.find_all('td')
 		for i in p2td:
 			results = str(i.text.strip())
 			if 'use ' in results:
-				results_list.append(results)	
+				clean = str(results.replace("\n",","))
+				super_clean = re.sub("[  ] ", "", clean)
+				results_list.append(super_clean)	
 
 		listToStr = ' '.join([str(i) for i in results_list])
 		active_cell = sheet.findall(URL)
@@ -113,23 +134,30 @@ def main():
 
 	def p3():
 		results_list = []
-		storep = soup.find_all('p', string = re.compile("store"))
-		for i in storep:
-			if i is not None:
-				results = (i.text.strip())
-				results_list.append(results)	
+		
+		p3p = soup.find_all('p')
+		for i in p3p:
+			results = str(i.text.strip())
+			if 'store' in results:
+				clean = str(results.replace("\n",","))
+				super_clean = re.sub("[  ] ", "", clean)
+				results_list.append(super_clean)	
 
 		p3li = soup.find_all('li')
 		for i in p3li:
 			results = str(i.text.strip())
 			if 'store' in results:
-				results_list.append(results)	
+				clean = str(results.replace("\n",","))
+				super_clean = re.sub("[  ] ", "", clean)
+				results_list.append(super_clean)	
 
 		p3td = soup.find_all('td')
 		for i in p3td:
 			results = str(i.text.strip())
 			if 'store' in results:
-				results_list.append(results)	
+				clean = str(results.replace("\n",","))
+				super_clean = re.sub("[  ] ", "", clean)
+				results_list.append(super_clean)	
 
 		listToStr = ' '.join([str(i) for i in results_list])
 		active_cell = sheet.findall(URL)
@@ -143,23 +171,30 @@ def main():
 
 	def p4():
 		results_list = []
-		protectp = soup.find_all('p', string = re.compile("protect"))
-		for i in protectp:
-			if i is not None:
-				results = (i.text.strip())
-				results_list.append(results)	
+		
+		p4p = soup.find_all('p')
+		for i in p4p:
+			results = str(i.text.strip())
+			if 'protect' in results:
+				clean = str(results.replace("\n",","))
+				super_clean = re.sub("[  ] ", "", clean)
+				results_list.append(super_clean)
 
 		p4li = soup.find_all('li')
 		for i in p4li:
 			results = str(i.text.strip())
 			if 'protect' in results:
-				results_list.append(results)	
+				clean = str(results.replace("\n",","))
+				super_clean = re.sub("[  ] ", "", clean)
+				results_list.append(super_clean)	
 
 		p4td = soup.find_all('td')
 		for i in p4td:
 			results = str(i.text.strip())
 			if 'protect' in results:
-				results_list.append(results)	
+				clean = str(results.replace("\n",","))
+				super_clean = re.sub("[  ] ", "", clean)
+				results_list.append(super_clean)	
 
 		listToStr = ' '.join([str(i) for i in results_list])
 		active_cell = sheet.findall(URL)
@@ -173,33 +208,41 @@ def main():
 
 	def p5():
 		results_list = []
-		sharep = soup.find_all('p', string = re.compile("share"))
-		for i in sharep:
-			if i is not None:
-				results = (i.text.strip())
-				results_list.append(results)
-
-		thirdp = soup.find_all('p', string = re.compile("third part"))
-		for i in thirdp:
-			if i is not None:
-				results = (i.text.strip())
-				results_list.append(results)
+		p5p = soup.find_all('p')
+		for i in p5p:
+			results = str(i.text.strip())
+			if 'share' in results:
+				clean = str(results.replace("\n",","))
+				super_clean = re.sub("[  ] ", "", clean)
+				results_list.append(super_clean)
+			if 'third part' in results:
+				clean = str(results.replace("\n",","))
+				super_clean = re.sub("[  ] ", "", clean)
+				results_list.append(super_clean)
 
 		p5li = soup.find_all('li')
 		for i in p5li:
 			results = str(i.text.strip())
 			if 'share' in results:
-				results_list.append(results)
+				clean = str(results.replace("\n",","))
+				super_clean = re.sub("[  ] ", "", clean)
+				results_list.append(super_clean)
 			if 'third part' in results:
-				results_list.append(results)
+				clean = str(results.replace("\n",","))
+				super_clean = re.sub("[  ] ", "", clean)
+				results_list.append(super_clean)
 
 		p5td = soup.find_all('td')
 		for i in p5td:
 			results = str(i.text.strip())
 			if 'share' in results:
-				results_list.append(results)
+				clean = str(results.replace("\n",","))
+				super_clean = re.sub("[  ] ", "", clean)
+				results_list.append(super_clean)
 			if 'third part' in results:
-				results_list.append(results)		
+				clean = str(results.replace("\n",","))
+				super_clean = re.sub("[  ] ", "", clean)
+				results_list.append(super_clean)		
 
 		listToStr = ' '.join([str(i) for i in results_list])
 		active_cell = sheet.findall(URL)
@@ -210,20 +253,29 @@ def main():
 		active_row = 'L' + row_str
 		sheet.update(active_row, listToStr)
 	p5()
+	
 	def contact():
-		results_list = []
-		for i in soup.find_all('a'):
+		contact_list = []
+		link = soup.find_all('a')
+		for i in link:
 			links = i.get('href')
-			if 'mailto:' in links:
-				results_list.append(links)
-		
-		listToStr = ' '.join([str(i) for i in results_list])
+			email_links = str(links)
+			if 'mailto:' in email_links:
+				contact_list.append(email_links)
+	
+		email_list = []
+		for i in contact_list:
+			if i not in email_list:
+				email_list.append(i)
+	
+		listToStr = ', '.join([str(i) for i in email_list])
+		clean = str(listToStr.replace("mailto:", ""))
 		active_cell = sheet.findall(URL)
 		for i in active_cell:
 			url_row = i.row
-			row_str = str(url_row)
-			
+			row_str = str(url_row)	
+	
 		active_row = 'M' + row_str
-		sheet.update(active_row, listToStr)
+		sheet.update(active_row, clean)
 	contact()
 main()
